3c7bcbe4533dc2bc88f2de6dafd3d3c83f753706
Ammar Khaku <ammar.khaku@gmail.com>
2011-08-17 23:01:17 -0400

    Initial commit. Everything is in a pretty rough state, lots of work to be done

---
+++ get_plugins.py
@@ -0,0 +1,16 @@
+import os
+
+plugins=[{"name":"supertab", "url":"https://github.com/tsaleh/vim-supertab.git"},
+        {"name":"nerdtree", "url":"git://github.com/scrooloose/nerdtree.git"},
+        {"name":"rails", "url":"git://github.com/tpope/vim-rails.git"}]
+
+bundle_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bundle/')
+
+print "Removing all current plugins!"
+os.system('rm -rf %s*' % bundle_dir)
+
+for plugin in plugins:
+    print "Downloading %s" % plugin['name']
+    os.system("git clone %s %s%s" % ( plugin['url'], bundle_dir, plugin['name'] ) )
+
+os.system("rm -rf %s*.git" % bundle_dir)
