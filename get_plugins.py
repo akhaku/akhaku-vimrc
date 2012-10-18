7a0288536e3bee37d29bde6cc12339fa2f254adb
Ammar Khaku <akhaku@evernote.com>
2012-10-18 14:37:13 -0700

    Add vim-less

--- get_plugins.py
+++ get_plugins.py
@@ -4,6 +4,7 @@ plugins=[{"name":"supertab", "url":"git://github.com/tsaleh/vim-supertab.git"},
         {"name":"nerdtree", "url":"git://github.com/scrooloose/nerdtree.git"},
         {"name":"command-t", "url":"git://git.wincent.com/command-t.git"},
         {"name":"vim-css-color", "url":"git://github.com/skammer/vim-css-color.git"},
+        {"name":"vim-less", "url":"https://github.com/groenewege/vim-less.git"},
         {"name":"rails", "url":"git://github.com/tpope/vim-rails.git"}]
 
 bundle_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bundle/')
@@ -15,6 +16,8 @@ for plugin in plugins:
     print "Downloading %s" % plugin['name']
     os.system("git clone %s %s%s" % ( plugin['url'], bundle_dir, plugin['name'] ) )
 
+os.system("rm -rf %s*.git" % bundle_dir)
+
 # Command-t C-extension setup
 command_t_ext_path = "%scommand-t/ruby/command-t/" % bundle_dir
 old_path = os.getcwd()
@@ -23,5 +26,10 @@ os.system("ruby extconf.rb")
 os.system("make")
 os.chdir(old_path)
 
-os.system("rm -rf %s*.git" % bundle_dir)
+# vim-css-color setup
+old_path = os.getcwd()
+os.chdir(bundle_dir)
+os.system("mv vim-css-color/after ../")
+os.chdir(old_path)
+
 print "Got all plugins"
