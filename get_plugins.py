5c65c04747277d6e0989db279155a0e554f95040
Ammar Khaku <ammar.khaku@gmail.com>
2018-02-05 19:13:02 -0800

    Use https, and fail on first error

--- get_plugins.py
+++ get_plugins.py
@@ -1,13 +1,13 @@
 import os
 
-plugins=[{"name":"supertab", "tag":"2.1", "url":"git://github.com/ervandew/vim-supertab.git"},
-        {"name":"nerdtree", "tag":"5.0.0", "url":"git://github.com/scrooloose/nerdtree.git"},
-        {"name":"command-t", "tag":"5.0.2", "url":"git://git.wincent.com/command-t.git"},
-        {"name":"vim-fugitive", "tag":"2.2", "url":"git@github.com:tpope/vim-fugitive.git"},
-        {"name":"vim-java-unused-imports", "tag":"v1.0.0", "url":"git://github.com/akhaku/vim-java-unused-imports.git"},
-        {"name":"vim-javascript", "tag":"v1.2.2", "url":"git://github.com/pangloss/vim-javascript.git"},
-        {"name":"vim-jsx", "tag":"master", "url":"git://github.com/mxw/vim-jsx.git"},
-        {"name":"vim-gitgutter", "tag":"master", "url":"git://github.com/airblade/vim-gitgutter.git"},
+plugins=[{"name":"supertab", "tag":"2.1", "url":"https://github.com/ervandew/supertab.git"},
+        {"name":"nerdtree", "tag":"5.0.0", "url":"https://github.com/scrooloose/nerdtree.git"},
+        {"name":"command-t", "tag":"5.0.2", "url":"https://github.com/wincent/command-t.git"},
+        {"name":"vim-fugitive", "tag":"v2.2", "url":"https://github.com/tpope/vim-fugitive.git"},
+        {"name":"vim-java-unused-imports", "tag":"v1.0.0", "url":"https://github.com/akhaku/vim-java-unused-imports.git"},
+        {"name":"vim-javascript", "tag":"v1.2.2", "url":"https://github.com/pangloss/vim-javascript.git"},
+        {"name":"vim-jsx", "tag":"master", "url":"https://github.com/mxw/vim-jsx.git"},
+        {"name":"vim-gitgutter", "tag":"master", "url":"https://github.com/airblade/vim-gitgutter.git"},
         {"name":"vim-surround", "tag":"v2.1", "url":"https://github.com/tpope/vim-surround.git"}]
 
 bundle_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bundle/')
@@ -17,7 +17,9 @@ os.system('rm -rf %s*' % bundle_dir)
 
 for plugin in plugins:
     print "Downloading %s" % plugin['name']
-    os.system("git clone --branch %s %s %s%s" % (plugin['tag'], plugin['url'], bundle_dir, plugin['name']) )
+    if os.system("git clone --branch %s %s %s%s" % (plugin['tag'], plugin['url'], bundle_dir, plugin['name'])) != 0:
+      print "Error downloading %s" % plugin['name']
+      exit(1)
 
 os.system("rm -rf %s*.git" % bundle_dir)
 
