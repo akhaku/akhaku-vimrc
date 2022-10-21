0877d1e7c431d2d16ec9bc4deff987497616cda5
Ammar Khaku <ammar.khaku@gmail.com>
2022-10-21 12:32:20 -0700

    Use python3 instead of python2 and upgrade commandT version

--- get_plugins.py
+++ get_plugins.py
@@ -2,7 +2,7 @@ import os
 
 plugins=[{"name":"supertab", "tag":"2.1", "url":"https://github.com/ervandew/supertab.git"},
         {"name":"nerdtree", "tag":"5.0.0", "url":"https://github.com/scrooloose/nerdtree.git"},
-        {"name":"command-t", "tag":"5.0.2", "url":"https://github.com/wincent/command-t.git"},
+        {"name":"command-t", "tag":"5.0.5", "url":"https://github.com/wincent/command-t.git"},
         {"name":"kotin-vim", "tag":"master", "url":"https://github.com/udalov/kotlin-vim.git"},
         {"name":"vim-fugitive", "tag":"v2.2", "url":"https://github.com/tpope/vim-fugitive.git"},
         {"name":"vim-java-unused-imports", "tag":"v1.0.0", "url":"https://github.com/akhaku/vim-java-unused-imports.git"},
@@ -15,23 +15,23 @@ plugins=[{"name":"supertab", "tag":"2.1", "url":"https://github.com/ervandew/sup
 
 bundle_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bundle/')
 
-print "Removing all current plugins!"
+print('Removing all current plugins!')
 os.system('rm -rf %s*' % bundle_dir)
 
 for plugin in plugins:
-    print "Downloading %s" % plugin['name']
-    if os.system("git clone --branch %s %s %s%s" % (plugin['tag'], plugin['url'], bundle_dir, plugin['name'])) != 0:
-      print "Error downloading %s" % plugin['name']
+    print(f'Downloading {plugin["name"]}')
+    if os.system(f'git clone --branch {plugin["tag"]} {plugin["url"]} {bundle_dir}{plugin["name"]}') != 0:
+      print(f'Error downloading {plugin["nlme"]}')
       exit(1)
 
-os.system("rm -rf %s*.git" % bundle_dir)
+os.system(f'rm -rf {bundle_dir}*.git')
 
 # Command-t C-extension setup
-command_t_ext_path = "%scommand-t/ruby/command-t/ext/command-t/" % bundle_dir
+command_t_ext_path = f'{bundle_dir}command-t/ruby/command-t/ext/command-t/'
 old_path = os.getcwd()
 os.chdir(command_t_ext_path)
 os.system("ruby extconf.rb")
 os.system("make")
 os.chdir(old_path)
 
-print "Got all plugins"
+print('Got all plugins')
