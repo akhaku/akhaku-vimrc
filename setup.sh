41a1f0e6dfa3a48da4c424de0a81cc73013f30f0
Ammar Khaku <ammar.khaku@gmail.com>
2025-07-16 15:39:42 -0700

    Overwrite symlinks if they exist

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ fi
 setup_vim_softlinks(){
     printf "Making soft links to .vim and .vimrc\n"
     current_dir=`pwd`
-    ln -s "$current_dir/vimrc" "$HOME/.vimrc"
-    ln -s "$current_dir" "$HOME/.vim"
+    ln -sf "$current_dir/vimrc" "$HOME/.vimrc"
+    ln -sf "$current_dir" "$HOME/.vim"
     printf "Done.\n\n"
 }
 
