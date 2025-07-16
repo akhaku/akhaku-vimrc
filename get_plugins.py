15ee83575387677fa64d01178afe7e3dcbe9474b
Ammar Khaku <ammar.khaku@gmail.com>
2025-07-16 15:14:51 -0700

    Use new typescript vim plugin branch name

--- get_plugins.py
+++ get_plugins.py
@@ -5,7 +5,7 @@ plugins=[{"name":"supertab", "tag":"2.1", "url":"https://github.com/ervandew/sup
         {"name":"vim-fugitive", "tag":"v3.7", "url":"https://github.com/tpope/vim-fugitive.git"},
         {"name":"vim-java-unused-imports", "tag":"v1.2.0", "url":"https://github.com/akhaku/vim-java-unused-imports.git"},
         {"name":"vim-javascript", "tag":"v1.2.2", "url":"https://github.com/pangloss/vim-javascript.git"},
-        {"name":"vim-gitgutter", "tag":"master", "url":"https://github.com/airblade/vim-gitgutter.git"},
+        {"name":"vim-gitgutter", "tag":"main", "url":"https://github.com/airblade/vim-gitgutter.git"},
         {"name":"typescript-vim", "tag":"master", "url":"https://github.com/leafgarland/typescript-vim.git"},
         {"name":"fzf", "tag":"master", "url":"https://github.com/junegunn/fzf.git"},
         {"name":"fzf.vim", "tag":"master", "url":"https://github.com/junegunn/fzf.vim.git"}]
