4771f770b865f86522755ca8430a32136a4d4562
Ammar Khaku <ammar.khaku@gmail.com>
2025-07-16 15:43:21 -0700

    Clean up install script

--- setup.sh
+++ setup.sh
@@ -1,40 +1,42 @@
 #!/bin/sh
 
-move_old_vim_files(){
-if [ -e "$HOME/.vim" ]; then
-    echo "Moving old .vim to .vim-old"
-    mv "$HOME/.vim" "$HOME/.vim-old" || true
-fi
+# Determine the directory where this script is located
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-if [ -e "$HOME/.vimrc" ]; then
-    echo "Moving old .vimrc to .vimrc-old"
-    mv "$HOME/.vimrc" "$HOME/.vimrc-old" || true
-fi
+move_old_vim_files() {
+    if [ -e "$HOME/.vim" ]; then
+        echo "Moving old .vim to .vim-old"
+        mv "$HOME/.vim" "$HOME/.vim-old" 2>/dev/null || echo "Could not move .vim (maybe .vim-old exists?)"
+    fi
 
+    if [ -e "$HOME/.vimrc" ]; then
+        echo "Moving old .vimrc to .vimrc-old"
+        mv "$HOME/.vimrc" "$HOME/.vimrc-old" 2>/dev/null || echo "Could not move .vimrc (maybe .vimrc-old exists?)"
+    fi
 }
 
-setup_vim_softlinks(){
+setup_vim_softlinks() {
     printf "Making soft links to .vim and .vimrc\n"
-    current_dir=`pwd`
-    ln -sf "$current_dir/vimrc" "$HOME/.vimrc"
-    ln -sf "$current_dir" "$HOME/.vim"
+    ln -sf "$SCRIPT_DIR/vimrc" "$HOME/.vimrc"
+    ln -sf "$SCRIPT_DIR" "$HOME/.vim"
     printf "Done.\n\n"
 }
 
-get_all_plugins(){
-    current_dir=`pwd`
-    printf "getting plugins now"
-    python "$current_dir/get_plugins.py"
+get_all_plugins() {
+    printf "getting plugins now\n"
+    if command -v python >/dev/null 2>&1; then
+        python "$SCRIPT_DIR/get_plugins.py"
+    else
+        echo "Python is not installed. Skipping plugin fetch."
+    fi
 }
 
-printf """
+printf "
 Ammar's Vimrc
 @author: Ammar Khaku (akhaku)
 Requires Vim7 compiled with ruby support for some of the ruby stuff
 
-"""
+"
 move_old_vim_files
 setup_vim_softlinks
 get_all_plugins
-
-
